920c24fc50c8d6e8c9e90464aacdb926910e3c6f
ajayvarshaan <varshaanajay@gmail.com>
2025-06-25 14:38:08 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/HR-platform-web-application/' 
+  base: '/HR-platform-web-application/'   // 👈 Exactly your GitHub repo name with slashes
 });
